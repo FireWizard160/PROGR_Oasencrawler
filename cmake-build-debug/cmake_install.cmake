0bf2602ed595fd301ce8517bb03c11a01cb187e3
Lukas Mayer <142104217+FireWizard160@users.noreply.github.com>
2024-02-21 22:55:44 +0100

    hardstuck

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/FH/SS24/PROGR/Oasencrawler
+# Install script for directory: C:/FH/SS24/PROGR/PROGR_Oasencrawler
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/FH/SS24/PROGR/Oasencrawler/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/FH/SS24/PROGR/PROGR_Oasencrawler/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ field.h
@@ -0,0 +1,11 @@
+
+#ifndef OASENCRAWLER_FIELD_H
+#define OASENCRAWLER_FIELD_H
+
+
+class field {
+
+};
+
+
+#endif //OASENCRAWLER_FIELD_H
